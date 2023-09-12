05f512b415e28d1175da6f1be4281f41a53f3f10
최수민 <jbsumin8424@gmail.com>
2023-09-12 19:40:21 +0900

    3rd

---
+++ clerk2/app/dashboard/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const DashboardPage = () => {
+  return (
+    <div>
+      <h1 className="text-2x1 font-bold mb-5">DashboardPage(대시보드)</h1>
+      <p className="mb-5">welcom to dashboard</p>
+    </div>
+  )
+}
+
+export default DashboardPage
